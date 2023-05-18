5fe38b673b77735e242d9076d2137cd0252b033a
Josh Hebert <jhebertcocktails@gmail.com>
2023-05-18 12:35:27 -0400

    work done with tutor

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,72 @@
+var today = dayjs();
+$('#currentDay').text(today.format('[Today is] dddd MMM D, YYYY'));
+var time = dayjs();
+$('#currentTime').text(today.format('[The time is] hh: mm A '));
+
+//Controls color changing on rows
+var rows = $(".row")
+let currentTime = parseInt(dayjs().format('HA'));
+
+$(".row").each(function(row) {  
+    let rowHour = parseInt(row.id) 
+    let textinput = $(this).children(".description")
+    console.log(textinput)
+    if (rowHour === currentTime) {
+          $(textinput).css("background-color", "red");
+    }
+  });
+
+// //Save to local storage
+localStorage.setItem()
+// //Button select
+$(document).ready(function(){
+$(".saveBtn").on("click", function() {
+var time = 
+var text =
+})
+})
+
+
+
+
+
+
+  //   else if (rowHour > currentTime) {
+  //   $(".description").css("background-color", "green");
+  //   }
+  //   if (currentTime === rowHour) {
+  // $(textinput).css("background-color", "red");
+  //   }
+  //    else if(currentTime > rowHour) {
+  //     $(textinput).css("background-color", "red");
+  //   } else if (currentTime < rowHour) {
+  //     $(textinput).css("background-color", "red")
+  //   }
+
+ 
+
+// function setColor(element, color) {
+//   element.classList.add("past");
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-$(function () {
+
+
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
@@ -20,4 +85,3 @@ $(function () {
   // attribute of each time-block be used to do this?
   //
   // TODO: Add code to display the current date in the header of the page.
-});
